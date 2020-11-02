a9df77d6ee9c18f64caed08caab91e526e422b34
FluffyMaguro <uminomaguro@gmail.com>
2020-11-03 00:35:09 +0100

    Working RTS filtering

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var app = {
 keywords: [],
+shown_keywords: [],
 lastFoundTerm: null,
 init: function() {
   document.querySelector('.reddit-dump').innerHTML = '';
@@ -44,7 +45,11 @@ startApp: function() {
 //Drop the text into the HTML
 addCommentstoHTML: function(text) {
   document.querySelector('.reddit-dump').innerHTML = text;
+  if (true) { //!!!rts init
+    app.keywords = data_rts
+  }
   highlight();
+  disableAll()
 },
 
 getCommentsFromJSON: function(json) {
@@ -59,7 +64,6 @@ getCommentsFromArray: function(arr, generation) {
   arr.forEach(function(item) {
     if (typeof item !== 'undefined') {
       text += '<div class="rpost gen'+generation+'" style="margin-left:'+generation*40+'px"><div class="rauthor">'+item.data.author + ' 🠉' + item.data.score + '</div>';                                  
-      
       
       let body = item.data.body;     
       if (body != null) {
@@ -99,19 +103,17 @@ getCommentsFromArray: function(arr, generation) {
 
 // Highlights text with given strings
 function highlight() {
-  console.log('HL:' + app.keywords)
+  
   let instance = new Mark(document.querySelector(".reddit-dump"));
   app.key_dict = new Object();
   app.last_parent = null;
   instance.unmark();
   hide_comments();
+
+  console.log('HL:', app.keywords);
+
   instance.mark(app.keywords, { //keywords
-      "synonyms": {           // synonyms
-          "WC3": "warcraft 3",
-          "SC": "starcraft",
-          "DOW": "dawn of war",
-          "HW": "halo wars"
-      },
+      "synonyms": data_synonyms,
        "separateWordSearch": false, // Search for whole things in mutli-word string
        "className": "highlighted",
        "filter": eachFilter,
@@ -123,18 +125,36 @@ function highlight() {
     }
   });
 
-  //Update keywords with numbers
+  //Add RTS keywords elements
+  for (const [key, value] of Object.entries(app.key_dict)) {
+      // console.log(key, value);
+        index = app.shown_keywords.indexOf(key); // check if it's shown
+        if (index < 0) {
+          createKeywordElement(key);
+        }
+    }
+
+  // Update search to those are were found (in RTS case)
+  app.keywords = [];
   let keywords = document.getElementsByClassName("keyword_d");
     for (i = 0; i < keywords.length; i++) {
-      let text = keywords[i].firstChild.textContent;
-      let span = keywords[i].getElementsByTagName('span')[0];
-      if (app.key_dict[text] != null) {
-        span.innerHTML = app.key_dict[text]
-      } else if (keywords[i].style.backgroundColor == keyword_deactivated_color) {
-      } else {
-        span.innerHTML = 0
+      if (keywords[i].style.backgroundColor == keyword_activated_color) {
+          app.keywords.push(keywords[i].firstChild.textContent)
       }
     }
+
+  //Update keywords with numbers
+  console.log(app.key_dict)
+  for (i = 0; i < keywords.length; i++) {
+    let text = keywords[i].firstChild.textContent;
+    let span = keywords[i].getElementsByTagName('span')[0];
+    if (app.key_dict[text] != null) {
+      span.innerHTML = app.key_dict[text]
+    } else if (keywords[i].style.backgroundColor == keyword_deactivated_color) {
+    } else if (span.innerHTML == null) {
+      span.innerHTML = 0
+    }
+  }
 }
 
 // Save last found term. This function runs before 'each'
@@ -151,7 +171,8 @@ function eachFound(el) {
 
   if (app.last_parent == parent) {return} // Skip if it's the same parent
   app.last_parent = parent;
-  parent.style.opacity = '1';
+  // parent.style.display = 'block';
+  parent.style.opacity = "1"
 
   if (app.key_dict[app.lastFoundTerm] == null) { //Update how many terms we found
       app.key_dict[app.lastFoundTerm] = 1
@@ -160,13 +181,17 @@ function eachFound(el) {
     }
 }
 
+
+
 function hide_comments(){ // Hides comments before filtering
   let comments = document.getElementsByClassName("rpost");
   for (i = 0; i < comments.length; i++) {
       if (app.keywords.length == 0) {
+        // comments[i].style.display = "block";
         comments[i].style.opacity = "1"
       } else {
-        comments[i].style.opacity = ".3"
+        // comments[i].style.display = "none";
+        comments[i].style.opacity = ".5"
       }
     }
 }
@@ -196,6 +221,16 @@ function addKeyword(){
   let text = keyInput.textContent;
   keyInput.innerHTML = '';
   if (text != null && text != '') {
+    createKeywordElement(text);
+
+    // Add to app keywords & update
+    app.keywords.push(text);
+    highlight();
+  }
+}
+
+function createKeywordElement(text) {
+ if (text != null && text != '') {
     
     // Update visuals
     let node = document.createElement("DIV");
@@ -216,10 +251,7 @@ function addKeyword(){
     bnode.onclick = function() {removeKeyword(text, node)};
     node.appendChild(bnode);
     document.getElementById("currentKeys").appendChild(node);
-
-    // Add to app keywords & update
-    app.keywords.push(text);
-    highlight();
+    app.shown_keywords.push(text);
   }
 }
 
@@ -236,14 +268,28 @@ function keywordClicked(key, node) {
     }
   }
   highlight();
+}
 
+function disableAll() { //disables all keyword buttons
+  app.keywords = [];
+  highlight();
+
+  let keywords = document.getElementsByClassName("keyword_d");
+    for (i = 0; i < keywords.length; i++) {
+      keywords[i].style.backgroundColor = keyword_deactivated_color;
+    }
 }
 
 function removeKeyword(key, node) {
   // remove from app keywords array
-  const index = app.keywords.indexOf(key);
+  let index = app.keywords.indexOf(key);
   if (index > -1) {
     app.keywords.splice(index, 1);
+  }
+
+  index = app.shown_keywords.indexOf(key);
+  if (index > -1) {
+    app.shown_keywords.splice(index, 1);
   }
   // Remove keyword element
   node.remove()
@@ -253,349 +299,5 @@ function removeKeyword(key, node) {
 }
 
 //DEBUGG!!!!
-document.getElementById("newKey").innerHTML = 'heroes';
-addKeyword();
-document.getElementById("newKey").innerHTML = 'level';
-addKeyword();
 
-var synonyms = {
-          "WC3": "warcraft 3",
-          "SC": "starcraft",
-          "DOW": "Warhammer 40,000: Dawn of War",
-          "HW": "Halo Wars",
-          "AoM": "Age of Mythology",
-          "AotS": "Ashes of the Singularity",
-          "C&C": "Command & Conquer",
-          "CoH": "Company of Heroes",
-          "BFME": "he Battle for Middle-earth",
-          "AoE": "Age of Empires",
-          "BW": "starcraft",
-          "SupCom":"Supreme Commander",
-          "UAW":"Universe at War",
-          "w40k":"Warhammer 40,000: Dawn of War"
-      };
 
-var rts = [
-'0 A.D',
- '7th Legion',
- '8-Bit Armies',
- '8-Bit Hordes',
- '8-Bit Invaders',
- 'A Year of Rain',
- 'ARSENAL Extended Power',
- 'ARSENAL: Taste the Power',
- 'Achron',
- 'Act of Aggression',
- 'Act of War: Direct Action',
- 'Act of War: High Treason',
- 'Against Rome',
- 'Age of Empires',
- 'Age of Mythology',
- 'Aggression: Reign Over Europe',
- 'Alexander',
- 'Alien Legacy',
- 'Alien Nations',
- 'Allegiance',
- 'America',
- 'American Conquest',
- 'Ancestors Legacy',
- 'Ancient Art of War in the Skies, The',
- 'Ancient Space',
- 'Ancient Wars: Sparta',
- 'Anno',
- 'Arena Wars',
- 'Armies of Exigo',
- 'Army Men: RTS',
- 'Ashes of the Singularity',
- 'Atrox',
- 'Axis and Allies',
- 'BANNERMEN',
- 'Baldies',
- 'Battle Command',
- 'Battle Isle: The Andosia War',
- 'Battle Realms',
- 'BattleForge',
- 'Battlefleet Gothic: Armada',
- 'Battles of the Outer Rim',
- 'Battlezone',
- 'Beasts and Bumpkins',
- 'Black Moon Chronicles',
- 'Blitzkrieg 3',
- 'Blood & Magic',
- 'Bokosuka Wars',
- 'Bos Wars',
- 'Cannon Fodder',
- 'Carrier Command',
- 'Castle Strike',
- 'Celtic Kings: Rage of War',
- 'Clash Royale',
- 'Colobot',
- 'Combat Leader',
- 'Command & Conquer',
- 'Command HQ',
- 'Command: Chains of War',
- 'Command: Modern Air Naval Operations',
- 'Company of Heroes',
- 'Conflict Zone',
- 'Confrontation: Peace Enforcement',
- 'Conquest Earth',
- 'Conquest: Frontier Wars',
- 'Cosmic Conquest',
- 'Cossacks',
- 'Counter Blow[6]',
- 'Creeper World',
- 'Crusader Kings',
- 'Cultures 2: The Gates of Asgard',
- 'Cultures: Discovery of Vinland',
- 'Cytron Masters',
- 'D-Day',
- 'DEFCON: Everybody Dies',
- 'Dark Colony',
- 'Dark Planet: Battle for Natrolis',
- 'Dark Reign',
- 'Darwinia',
- 'Dawn of Fantasy',
- 'Desert Rats vs Afrika Korps',
- 'Dogs of War: Battle On Primus IV',
- 'Dominant Species',
- 'Dominion: Storm Over Gift 3',
- 'Dragon Force',
- 'Dragon Throne: Battle of Red Cliffs',
- 'Dune 2000',
- 'Dune II',
- 'Dungeons & Dragons: Dragonshard',
- 'Earth 2140',
- 'Earth 2150',
- 'Earth 2160',
- 'Emperor: Battle for Dune',
- 'Empire Earth',
- 'Empires Apart',
- 'Empires: Dawn of the Modern World',
- 'Enemy Nations',
- 'Energy Warrior',
- 'Etherium',
- 'Europa Universalis',
- 'Evil Genius',
- 'Evolution RTS',
- 'Executive Assault',
- 'Eximius: Seize the Frontline',
- 'Exodus: The Last War',
- 'Extreme Tactics',
- 'Faces of War',
- 'Fate of Hellas',
- 'Final Conflict',
- 'Forged Battalion',
- 'Foundation',
- 'Fragile Allegiance',
- 'Gain Ground',
- 'Gangland',
- 'Genesis Rising: The Universal Crusade',
- 'Genewars',
- 'Genius: Biology',
- 'Giants: Citizen Kabuto',
- 'Glest',
- 'Globulation 2',
- 'Great Invasions',
- 'Grey Goo',
- 'GrimGrimoire',
- 'Ground Control',
- 'HYPERNOVA: Escape from Hadea',
- 'Haegemonia: Legions of Iron',
- 'Halo Wars',
- 'Hanjuku Eiyuu - Aa Sekai Yo Hanjuku Nare',
- 'Hearts of Iron',
- 'Hegemony Gold: Wars of Ancient Greece',
- 'Hegemony III: Clash of the Ancients',
- 'Hegemony Rome: The Rise of Caesar',
- 'Heroes of Annihilated Empires',
- 'Heroes of Mana',
- 'Herzog',
- 'Herzog Zwei',
- 'Highland Warriors',
- 'Homeworld',
- 'Hostile Waters: Antaeus Rising',
- 'Hundred Swords',
- 'I of the Dragon',
- 'Illyriad',
- 'Imperator: Rome',
- 'Imperium Galactica',
- 'Imperivm: Great Battles of Rome',
- 'Impossible Creatures',
- 'Inkawar',
- 'Invasion',
- 'Iron Harvest',
- 'Iron Seed',
- "J.R.R. Tolkien's Riders of Rohan",
- "J.R.R. Tolkien's War in Middle-earth",
- "Jeff Wayne's The War of the Worlds",
- 'Judgment: Apocalypse Survival Simulation',
- 'Jurassic War',
- 'K240',
- 'KKND',
- 'Kingdom Under Fire: A War of Heroes',
- 'Kingdom Wars 2: Battles',
- 'KnightShift',
- 'Knights and Merchants:',
- 'Knights of Honor',
- 'Kohan',
- 'Konung 2: Blood of Titans',
- 'L.E.D. Wars, The',
- 'LEGO Rock Raiders',
- 'Legionnaire',
- 'Lord of the Rings',
- 'The Battle for Middle-earth',
- 'Lords of Everquest',
- 'Loria',
- 'LostMagic',
- 'MAD: Global Thermonuclear War',
- 'Machines',
- 'Maelstrom',
- 'Magic and Mayhem',
- 'Magic and Mayhem: The Art of Magic',
- 'Magnant',
- 'Majesty 2: The Fantasy Kingdom Sim',
- 'Majesty: The Fantasy Kingdom Sim',
- 'Majesty: The Northern Expansion',
- 'Malvinas 2032',
- 'Mayday: Conflict Earth',
- 'Mech Platoon',
- 'Medieval II: Total War',
- 'Medieval: Total War',
- 'Mega Lo Mania',
- 'MegaGlest',
- 'Men of War',
- 'Meridian: New World',
- 'Meridian: Squad 22',
- 'Metal Fatigue',
- 'Metal Marines',
- 'Micro Commandos',
- 'Mission: Humanity',
- 'Modem Wars',
- 'Myth II: Soulblighter',
- 'Myth III: The Wolf Age',
- 'Myth: The Fallen Lords',
- 'NATO Commander',
- 'Napoleon',
- 'Naval War: Arctic Circle',
- 'Nemesis of the Roman Empire',
- 'NetStorm: Islands At War',
- 'Nether Earth',
- 'Nexus: The Jupiter Incident',
- 'Nightside',
- "No Man's Land",
- 'North & South',
- 'Northgard',
- 'Ocean Battle',
- 'Offworld Trading Company',
- 'Original War',
- 'Outlive',
- 'Outpost 2: Divided Destiny',
- 'ParaWorld',
- 'Pax Imperia Eminent Domain',
- 'Perimeter',
- 'Persian Wars',
- 'Pikmin',
- 'Planet X2',
- 'Planetary Annihilation',
- 'Populous',
- 'Powermonger',
- 'Praetorians',
- 'Psi-5 Trading Company',
- 'R.U.S.E',
- 'Rage of Mages',
- 'Re-Legion',
- 'Real War',
- 'Red Odyssey, The',
- 'Reunion',
- 'Rise and Fall: Civilizations at War',
- 'Rise of Nations',
- 'Rising Kingdoms',
- 'Rising Lands',
- 'Rival Realms',
- 'Riverworld',
- 'Rome: Total War',
- 'Rusted Warfare - RTS',
- 'S.W.I.N.E.',
- 'Sacrifice',
- 'Sangokushi Battlefield',
- 'Savage: The Battle for Newerth',
- 'Sea Battle',
- 'Sengoku',
- 'Settlers',
- 'Seven Kingdoms',
- 'Shadow Sorcerer',
- 'Shogun: Total War',
- 'Siege',
- 'Sins of a Solar Empire',
- 'Skull Caps',
- 'Smiley Commandos',
- 'Soldiers: Heroes of World War II',
- 'Space Colony',
- 'SpellForce',
- 'Spring 1944',
- 'Spring project',
- 'Stalin vs. Martians',
- 'Star Command',
- 'Star Trek',
- 'Star Wars',
- 'Brood War',
- 'StarCraft',
- 'Starship Troopers',
- 'State of War',
- 'Steel Division 2',
- 'Steel Division: Normandy 44',
- 'Stellar Warfare',
- 'Stonkers',
- 'Stronghold',
- 'Submarine Titans',
- 'Sudden Strike',
- 'SunAge',
- 'Supremacy 1914',
- 'Supremacy: Your Will Be Done',
- 'Supreme Commander',
- 'Supreme Ruler 2010',
- 'Supreme Ruler 2020',
- 'Svea Rike',
- 'Svea Rike 2',
- 'Swords & Soldiers',
- 'Tanktics',
- 'Taste of Power',
- 'Thandor: The Invasion',
- 'The Ancient Art of War',
- 'The Golden Horde',
- 'The Nations',
- 'Theatre of War',
- 'Theocracy',
- 'They Are Billions',
- 'Three Kingdoms',
- 'Tone Rebellion, The',
- 'Tooth and Tail',
- 'Total Annihilation',
- 'Total War',
- 'Two Thrones',
- 'Tzar: Burden of the Crown',
- 'Universe at War',
- 'Urban Assault',
- 'Utopia',
- 'Victoria: An Empire Under the Sun',
- 'War Front: Turning Point',
- 'War Incorporated',
- 'War Party',
- 'War Wind',
- 'War of Legends',
- 'War of Nerves!',
- 'WarBreeds',
- 'Warcraft',
- 'Wargame',
- 'Warhammer 40,000: Dawn of War',
- 'Dawn of War',
- 'Warpath',
- 'Warrior Kings',
- 'Wars and Warriors',
- 'Warzone 2100',
- 'World War III',
- 'World in Conflict',
- 'WorldShift',
- 'Z: Steel Soldiers',
- 'Zero-K',
- 'reconquest']
